5ca84e6431f095d9ab7a2b1091c4170f994df3cd
karvannanr <karrbtech@gmail.com>
2014-09-27 00:05:25 +0530

    Folder structure changed

--- Message/src/com/newfeature/msg/util/SmsUtils.java
+++
@@ -1,7 +0,0 @@
-package com.newfeature.msg.util;
-
-public enum SmsUtils {
-	INSTANCE;
-	
-//	public static final String inboxQuery = "SELECT MESSAGES.*, C.MSG_COUNT (SELECT THREAD_ID, COUNT(*) AS MSG_COUNT, MAX(DATE) AS MSG_MAX FROM " + + " GROUP BY THREAD_ID) AS C, " ++ " WHERE C.THREAD_ID = MESSAGES.THREAD_ID AND MESSAGES.DATE = C.DATE";
-}
