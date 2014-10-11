c7ff49ec800dc7c70c18cec173d958dd09395402
Naveen <navee.subramanian@imaginea.com>
2014-10-11 11:47:49 +0530

    Initial commit 1.0.X branch

---
+++ src/com/newfeature/msg/util/MessageConstants.java
@@ -0,0 +1,15 @@
+package com.newfeature.msg.util;
+
+public class MessageConstants {
+
+	// Prasanna 172.17.0.71:4567
+	//
+
+	public static final String SERVER_HOST = "http://208873da.ngrok.com/";
+
+	public static final String FETCH_DEALS_URL = SERVER_HOST
+			+ "fetchDeals?deviceId=";
+
+	public static final String SAVE_MESSAGE_URL = SERVER_HOST + "saveMessage";
+
+}
